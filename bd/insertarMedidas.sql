76cb753cee86168d88b49edc7110dec9aa015558
David09x <71710020+David09x@users.noreply.github.com>
2021-10-17 17:02:26 +0200

    Delete bd directory

--- bd/insertarMedidas.sql
+++
@@ -1,5 +0,0 @@
--- insertarMedidas.sql
-insert into Medidas values ("123845434", 20.25, 17);
-insert into Medidas values ("201234573", 25.20, 18);
-insert into Medidas values ("201234588", 22.34, 19);
-insert into Medidas values ("201234598", 24.56, 20);
--- bd/medidas.sql
+++
@@ -1,7 +0,0 @@
--- Medidas.sql
-create table Medidas (
-id TEXT(9) not null,
-valores REAL(4) not null,
-hora INTEGER(4) not null,
-primary key (id)
-);
